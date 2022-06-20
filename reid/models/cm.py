b83d315621e91ef8df70cf4ea372dc02b2655bdd
HongweiZhang97 <32814159+HongweiZhang97@users.noreply.github.com>
2022-06-20 18:45:01 +0800

    Update cm.py

--- reid/models/cm.py
+++ reid/models/cm.py
@@ -4,7 +4,6 @@ from abc import ABC
 import torch
 import torch.nn.functional as F
 from torch import nn, autograd
-from ..utils.pair_att import pair_att_sims, min_dim_sims
 
 from torch.autograd import Variable, Function
 
